9c3c852c87d2b81f76703977a7165d72dbb13277
Attila Szabo <aszabo@vrvis.at>
2021-07-06 11:30:31 +0200

    region growing

--- src/quadtree-test/Program.fs
+++ src/quadtree-test/Program.fs
@@ -45,7 +45,10 @@ module QuadTree =
     let getData (q : QuadTree) =
         match q with 
         | Leaf d -> d
-        | Node (d,_,_,_,_) -> d
+        | Node(d,_,_,_,_) -> d
+
+    let path (q : QuadTree) =
+        (getData q).location
 
     let generate leafThreshold =
         ct <- 0
@@ -56,15 +59,15 @@ module QuadTree =
             if density <= leafThreshold then Leaf {idx = idx(); density = density; parent = None; location = locationCode; level = level}
             else
                 let parts = Array.init 4 (fun _ -> randBetween 0.0 density) |> Array.sort
-                let bl = build (2::locationCode) parts.[0] (level+1)
-                let tl = build (0::locationCode) (parts.[1]-parts.[0]) (level+1)
-                let tr = build (1::locationCode) (parts.[2]-parts.[1]) (level+1)
-                let br = build (3::locationCode) (parts.[3]-parts.[2]) (level+1)
+                let bl = build (locationCode@[2]) parts.[0] (level+1)
+                let tl = build (locationCode@[0]) (parts.[1]-parts.[0]) (level+1)
+                let tr = build (locationCode@[1]) (parts.[2]-parts.[1]) (level+1)
+                let br = build (locationCode@[3]) (parts.[3]-parts.[2]) (level+1)
                 Node({idx = idx(); density = density; parent = None; location = locationCode; level = level}, bl, tl, tr, br)
         let rec fixParents (p : Option<QuadTree>) (q : QuadTree) =
             match q with 
             | Leaf d -> d.parent <- p
-            | Node (d, bl, tl, tr, br) -> 
+            | Node(d, bl, tl, tr, br) -> 
                 d.parent <- p
                 fixParents (Some q) bl
                 fixParents (Some q) tl
@@ -99,16 +102,16 @@ module QuadTree =
                   V2d(ma.X,mi.Y);V2d(mi.X,mi.Y);
                 |]
                 pos, posred d, posylo d
-            | Node (d, bl, tl, tr, br) -> 
+            | Node(d, bl, tl, tr, br) -> 
                 let r = ma-mi
                 let p00 = mi
-                let p01 = V2d(mi.X + r.X/2.0, mi.Y)
-                let p02 = V2d(ma.X,           mi.Y)
-                let p10 = V2d(mi.X,           mi.Y + r.Y/2.0)
+                let p10 = V2d(mi.X + r.X/2.0, mi.Y)
+                let p20 = V2d(ma.X,           mi.Y)
+                let p01 = V2d(mi.X,           mi.Y + r.Y/2.0)
                 let p11 = V2d(mi.X + r.X/2.0, mi.Y + r.Y/2.0)
-                let p12 = V2d(ma.X,           mi.Y + r.Y/2.0)
-                let p20 = V2d(mi.X,           ma.Y)
-                let p21 = V2d(mi.X + r.X/2.0, ma.Y)
+                let p21 = V2d(ma.X,           mi.Y + r.Y/2.0)
+                let p02 = V2d(mi.X,           ma.Y)
+                let p12 = V2d(mi.X + r.X/2.0, ma.Y)
                 let p22 = ma
                 let (pa0,pr0,py0) = traverse p00 p11 tl t
                 let (pa1,pr1,py1) = traverse p10 p21 tr t
@@ -122,7 +125,7 @@ module QuadTree =
             let res = traverse mi ma q t
             res
 
-        let ps = AVal.map2 (fun q t -> positions V2d.NN V2d.II q t) quad traversal
+        let ps = AVal.map2 (fun q t -> positions (V2d(-1.0,1.0)) (V2d(1.0,-1.0)) q t) quad traversal
 
         let whiteBoxes = ps |> AVal.map (fun (d,_,_) -> d)
         let redTris =    ps |> AVal.map (fun (_,d,_) -> d)
@@ -195,8 +198,8 @@ module QuadTree =
             match q with 
             | Leaf _ -> 
                 if i = 0 then [q] else []
-            | Node (_,bl,tl,tr,br) -> 
-                if i = 0 then [bl;tl;tr;br] else
+            | Node(_,bl,tl,tr,br) -> 
+                if i = 0 then [q] else
                     List.concat [
                         traverse (i-1) bl
                         traverse (i-1) tl
@@ -218,7 +221,7 @@ module QuadTree =
 
 module Traversal =
     // http://web.archive.org/web/20120907211934/http://ww1.ucmss.com/books/LFS/CSREA2006/MSV4517.pdf
-    let getNeighbor (d : Dir) (q : QuadTree) =
+    let getNeighbor (d : Dir) (q : QuadTree) (root : QuadTree) =
         match (QuadTree.getData q).location with 
         | [] -> None
         | l -> 
@@ -256,58 +259,69 @@ module Traversal =
                                 | Up    -> 1,None
                             | _ -> failwith ""
                         constructPath newDir remaining 
-                        |> Option.map (fun res -> newLoc::res)
+                        |> Option.map (fun res -> res@[newLoc])
                     | [] -> None
-            constructPath (Some d) l
+            constructPath (Some d) (List.rev l)
             |> Option.bind (fun path ->
-                QuadTree.findPath path q
+                QuadTree.findPath path root
             )
 
-    let regionGrowFromRandomStart (startLevel : int) (q : QuadTree) (resCb : Traversed -> unit) =
+    let regionGrowFromRandomStart (startLevel : int) (root : QuadTree) (resCb : Traversed -> unit) =
+
         let rand = RandomSystem()
         let startNode =
-            let nodes = QuadTree.nodesOfLevel startLevel q |> List.toArray
-            nodes.[rand.UniformInt(nodes.Length-1)]
+            let nodes = QuadTree.nodesOfLevel startLevel root |> List.toArray
+            nodes.[rand.UniformInt(nodes.Length)]
         let mutable finished = MapExt.empty
 
-        let queue = Queue<QuadTree>()
+        let queue = LinkedList<QuadTree>()
         let tryEnqueue (q : QuadTree) =
-            if not (finished |> MapExt.containsKey (QuadTree.getData q).idx) then queue.Enqueue q
+            if not (finished |> MapExt.containsKey (QuadTree.getData q).idx) then queue.AddLast q |> ignore
+        let tryEnqueueFront (q : QuadTree) =
+            if not (finished |> MapExt.containsKey (QuadTree.getData q).idx) then queue.AddFirst q |> ignore
+
+        let dequeue() =
+            let res = queue.First.Value
+            queue.RemoveFirst()
+            res
 
         tryEnqueue startNode
         
         let rec tryEnqueueNeighbor (d : Dir) (q : QuadTree) =
-            match getNeighbor d q with 
+            match getNeighbor d q root with 
             | Some n -> tryEnqueue n
             | None -> 
                 match (QuadTree.getData q).parent with 
-                | Some p -> tryEnqueueNeighbor d p
+                | Some p -> 
+                    Log.line "try collapse %A" (QuadTree.path p)
+                    tryEnqueueNeighbor d p
                 | None -> ()
 
         while not (queue.IsEmpty()) do
-            Thread.Sleep 150
-            match queue.TryDequeue() with
-            | (true,q) -> 
-                let mutable skip = false
-                if (QuadTree.getData q).level < startLevel then 
-                    match q with 
-                    | Leaf _ -> ()
-                    | Node (_,bl,tl,tr,br) -> 
-                        tryEnqueue bl
-                        tryEnqueue tl
-                        tryEnqueue tr
-                        tryEnqueue br
-                        skip <- true
+            let q = dequeue()
+            let mutable skip = false
+            if (QuadTree.getData q).level < startLevel then 
+                match q with 
+                | Leaf _ -> ()
+                | Node(_,bl,tl,tr,br) -> 
+                    Log.line "try split %A" (QuadTree.path q)
+                    tryEnqueue bl
+                    tryEnqueue tl
+                    tryEnqueue tr
+                    tryEnqueue br
+                    skip <- true
 
-                if not skip then 
-                    finished <- finished |> MapExt.add (QuadTree.getData q).idx true
-
-                    tryEnqueueNeighbor Left q
-                    tryEnqueueNeighbor Up q
-                    tryEnqueueNeighbor Right q
-                    tryEnqueueNeighbor Down q
+            if not skip then 
+                finished <- finished |> MapExt.add (QuadTree.getData q).idx true
                 resCb {startIdx = (QuadTree.getData startNode).idx; visited = finished}
-            | _ -> ()
+                Thread.Sleep 5
+
+                tryEnqueueNeighbor Left q
+                tryEnqueueNeighbor Up q
+                tryEnqueueNeighbor Right q
+                tryEnqueueNeighbor Down q
+                
+        Log.line "done"
         resCb {startIdx = (QuadTree.getData startNode).idx; visited = finished}
 
 [<EntryPoint;STAThread>]
@@ -315,7 +329,7 @@ let main argv =
     let mutable ver = 0
     let create() =
         Log.startTimed "generate ..."    
-        let res = QuadTree.generate 0.001
+        let res = QuadTree.generate 0.005
         ver <- ver+1
         Log.stop()
         res
@@ -330,10 +344,10 @@ let main argv =
             do! Async.SwitchToNewThread()
             let mutable lastVer = ver
             while true do 
-                do! Async.Sleep 50
+                do! Async.Sleep 25
                 if lastVer <> ver then 
                     lastVer <- ver 
-                    Traversal.regionGrowFromRandomStart 1 (q |> AVal.force) (fun t -> transact (fun _ -> traversed.Value <- t))
+                    Traversal.regionGrowFromRandomStart 4 (q |> AVal.force) (fun t -> transact (fun _ -> traversed.Value <- t))
         } |> Async.Start
 
     let sg = QuadTree.toSg q traversed
